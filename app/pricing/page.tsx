0244744b79901ad29f1c37647bc4e1fc7e6f452e
DevDivyansh <576divyanshsharma@gmail.com>
2026-09-18 23:51:46 +0530

    feat(payment): integrate Stripe for payment processing and add pricing tiers Frontend Part

---
+++ app/pricing/page.tsx
@@ -0,0 +1,45 @@
+import { requireAuth } from '@/lib/auth-guard'
+import { PricingCards } from '@/components/ui/pricing-cards'
+
+const pricingTiers = [
+  {
+    id: "free",
+    name: "Free",
+    price: 0,
+    priceId: null,
+    currency: "USD",
+    interval: "month" as const,
+    features: [
+      "Access to Basic features",
+      "Limited Usage",
+      "Community Support",
+    ],
+    isPopular: false,
+  },
+  {
+    id: "premium",
+    name: "Premium",
+    price: 10,
+    priceId: "premium",
+    currency: "USD",
+    interval: "month" as const,
+    features: [
+      "Access to Premium features",
+      "Unlimited Usage",
+      "Priority support",
+      "Community Support",
+    ],
+    isPopular: true,
+  },
+];
+
+const page = async () => {
+    await requireAuth();
+  return (
+    <main className="min-h-screen">
+      <PricingCards tiers={pricingTiers} />
+    </main>
+  )
+}
+
+export default page
---
+++ lib/stripe.ts
@@ -0,0 +1,9 @@
+import stripe from "stripe";
+
+export const stripeClient = new stripe(process.env.STRIPE_SECRET_KEY!);
+
+export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+
+export const STRIPE_PRICE_IDs = {
+  premium: "price_1UH5Jj5zPSK9N6tg35VBdN3b",
+} as const;
